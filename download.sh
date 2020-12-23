208d4e237800c21c3d9a4bbc5168df90ef769ac6
reinwangops <76520811+reinwangops@users.noreply.github.com>
2020-12-23 20:48:59 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/reinwangops/cdn_deploy/main/cdn_new_h5_update.sh
+wget https://raw.githubusercontent.com/reinwangops/cdn_deploy/main/cdn_pc_update.sh
+wget https://raw.githubusercontent.com/reinwangops/cdn_deploy/main/template.txt
