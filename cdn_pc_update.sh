a082c47d0c2de004adf03f197a03239d2bad3303
reinwangops <76520811+reinwangops@users.noreply.github.com>
2020-12-23 20:04:26 +0800

    Create cdn_pc_update.sh

---
+++ cdn_pc_update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SRC_PATH=/home/development/front
+SRC_PATH=/home/development/new_h5/replace_prod
+PRD_PATH=/home/nginx/web/replace
+PKG_NAME=cdndist.zip
+SRC_NAME=dist
+tartime=`date "+%Y%m%d_%H%M%S"`
+
+#check backup dir
+if [ ! -d "$SRC_PATH/pc_backup" ]; then
+  mkdir $SRC_PATH/pc_backup
+fi
+
+#check nginx dir
+if [ ! -d "$PRD_PATH" ]; then
+  mkdir $PRD_PATH
+fi
+
+#copy && backup
+cp -rf $SRC_PATH/$PKG_NAME $PRD_PATH
+cp -rf $SRC_PATH/$PKG_NAME $SRC_PATH/pc_backup/$PKG_NAME\_$tartime
+
+#rollbackneed && update
+cd $PRD_PATH
+rm -rf bk-pc
+mv pc bk-pc
+unzip $PKG_NAME
+mv $SRC_NAME pc
+rm -rf  pc/index.html
