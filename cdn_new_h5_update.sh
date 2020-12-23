06c153e92bd561d891f7b9baf74725acdfe9825a
reinwangops <76520811+reinwangops@users.noreply.github.com>
2020-12-23 20:03:36 +0800

    Create cdn_new_h5_update.sh

---
+++ cdn_new_h5_update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SRC_PATH=/home/development/front
+SRC_PATH=/home/development/new_h5/replace_prod
+PRD_PATH=/home/nginx/web/replace
+PKG_NAME=distcdn.zip
+SRC_NAME=distcdn
+tartime=`date "+%Y%m%d_%H%M%S"`
+
+#check backup dir
+if [ ! -d "$SRC_PATH/h5_backup" ]; then
+  mkdir $SRC_PATH/h5_backup
+fi
+
+#check nginx dir
+if [ ! -d "$PRD_PATH" ]; then
+  mkdir $PRD_PATH
+fi
+
+#copy && backup
+cp -rf $SRC_PATH/$PKG_NAME $PRD_PATH
+cp -rf $SRC_PATH/$PKG_NAME $SRC_PATH/h5_backup/$PKG_NAME\_$tartime
+
+#rollbackneed && update
+cd $PRD_PATH
+rm -rf bk-h5
+mv h5 bk-h5
+unzip $PKG_NAME
+mv $SRC_NAME h5
+rm -rf  h5/index.html
