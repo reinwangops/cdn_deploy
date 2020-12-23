731d92b79a3db10db5af491a4c6940421d732522
reinwangops <76520811+reinwangops@users.noreply.github.com>
2020-12-23 21:52:21 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,2 @@
 wget -q https://raw.githubusercontent.com/reinwangops/cdn_deploy/main/cdn_new_h5_update.sh
 wget -q https://raw.githubusercontent.com/reinwangops/cdn_deploy/main/cdn_pc_update.sh
-wget -q https://raw.githubusercontent.com/reinwangops/cdn_deploy/main/template.txt
