c9d6746f9a9f276e4ea59d7db4f7294fe577e117
reinwangops <76520811+reinwangops@users.noreply.github.com>
2020-12-24 16:01:03 +0800

    Update cdn_new_h5_update.sh

--- cdn_new_h5_update.sh
+++ cdn_new_h5_update.sh
@@ -3,13 +3,13 @@
 #SRC_PATH=/home/development/front
 SRC_PATH=/home/development/new_h5/replace_prod
 PRD_PATH=/home/nginx/web/replace
-PKG_NAME=cdndist.zip
-SRC_NAME=dist
+PKG_NAME=distcdn.zip
+SRC_NAME=distcdn
 tartime=`date "+%Y%m%d_%H%M%S"`
 
 #check backup dir
-if [ ! -d "$SRC_PATH/pc_backup" ]; then
-  mkdir $SRC_PATH/pc_backup
+if [ ! -d "$SRC_PATH/h5_backup" ]; then
+  mkdir $SRC_PATH/h5_backup
 fi
 
 #check nginx dir
@@ -19,12 +19,12 @@ fi
 
 #copy && backup
 \cp -rf $SRC_PATH/$PKG_NAME $PRD_PATH
-\cp -rf $SRC_PATH/$PKG_NAME $SRC_PATH/pc_backup/$PKG_NAME\_$tartime
+\cp -rf $SRC_PATH/$PKG_NAME $SRC_PATH/h5_backup/$PKG_NAME\_$tartime
 
 #rollbackneed && update
 cd $PRD_PATH
-rm -rf bk-pc
-\mv -f pc bk-pc
+rm -rf bk-h5
+\mv -f h5 bk-h5
 unzip $PKG_NAME
-\mv -f $SRC_NAME pc
-rm -rf  pc/index.html
+\mv -f $SRC_NAME h5
+rm -rf  h5/index.html
